0ca62fa4ea8d2f8742b99705456ab0c94cfaedd1
Enhon <yinhao19921119@163.com>
2018-04-09 02:21:11 +0800

    Next Array

--- src/main/java/enhon/stringmatch/StringMatch.java
+++ src/main/java/enhon/stringmatch/StringMatch.java
@@ -1,10 +1,44 @@
 package enhon.stringmatch;
 
+import java.util.Arrays;
+
 public class StringMatch {
 
 
     public static void main(String[] args) {
+        System.out.println(Arrays.toString(getNextArray("abaabcaba")));
+    }
 
+
+
+    public static int[]  getNextArray(String str){
+        char[] charArray = str.toCharArray();
+        int temp[]=new int[str.length()];
+        String tempStr=null;
+        int tempIndex=0;
+        for(int i=0;i<str.length();++i){
+            if(i==0){
+                temp[0]=-1;
+            }else if(i==1){
+                temp[1]=0;
+            }else{
+                tempStr=str.substring(0,i);
+                tempIndex=1;
+                while((!tempStr.substring(0,tempStr.length()-tempIndex).equals(tempStr.substring(tempIndex,tempStr.length())))){
+                    tempIndex++;
+                    if((tempStr.length()-tempIndex)<1){
+                        break;
+                    }
+                }
+                if((tempStr.length()-tempIndex)<1){
+                    temp[i]=0;
+                }else{
+                    temp[i]=tempStr.substring(0,tempStr.length()-tempIndex).length();
+                }
+            }
+
+        }
+        return temp;
     }
 
 
